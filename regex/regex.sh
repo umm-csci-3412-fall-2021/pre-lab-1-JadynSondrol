9aa83c1e2fa8536b64bbe980c4c345b47e77a3c3
Jadyn Sondrol <sondr037@morris.umn.edu>
2021-09-07 12:17:21 -0500

    This commit adds the regex exp. for r0, r1, r2

---
+++ regex/regex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Given the example in the prelab this takes a list of two elements, the first is a name
+# the second is a food, and then puts them in a list starting with the name
+sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt > r0_output.txt
+
+# This takes 2 sentences, the first is I am ... with and name and the second is my favorite sandwich is ...
+# with a sandwich. Then It returns them in a list 1. name and 2. favorite sandwich. The p at the end removes the non 
+# matching part
+sed -En 's/\* I am ([a-zA-Z]+). My favorite sandwich is ([a-zA-Z]+)\./1 \1\n2 \2\n/p' <r1_input.txt > r1_output.txt
+
+sed -E 's/\* sandwich with ([a-zA-Z\.]+) ([a-zA-Z ]+)/1. \1\n2. \2\n/' <r2_input.txt > r2_output.txt
+
+
