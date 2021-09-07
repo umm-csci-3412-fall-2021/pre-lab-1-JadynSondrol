8802cc07189ffd8e12e7e68905ad0a9ed071008d
Jadyn Sondrol <sondr037@morris.umn.edu>
2021-09-07 10:56:56 -0500

    This allows you to wrap test
    Specifically in between a header and a footer

---
+++ wrap_contents/wrap_contents.sh
@@ -0,0 +1,19 @@
+# the name for the file that need to be wrapped
+FILENAME=$1
+
+# the name used to specify to header/footer
+SPECIFIED=$2
+
+# the name of the resulting file
+RESULT=$3
+
+# gets the correct name of the header
+HEADERNAME="${SPECIFIED}_header.html"
+
+# gets the correct name of the footer
+FOOTERNAME="${SPECIFIED}_footer.html"
+
+# copies the contents of the header, file and the footer into the result file
+cat "$HEADERNAME" "$FILENAME" "$FOOTERNAME" > "$RESULT"
+
+
